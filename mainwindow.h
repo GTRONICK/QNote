8332a07317d52c5e433c7cde8f113ca09ee34b34
Jaime Quiroga <gtronick@gmail.com>
2017-07-28 08:15:38 -0500

    Added multi-buffer signals and slots

--- mainwindow.h
+++ mainwindow.h
@@ -56,6 +56,22 @@ private slots:
     void main_slot_currentLineChanged();
     void on_actionFont_triggered();
     void on_actionAlways_on_top_triggered(bool checked);
+    void main_slot_showHideMenuBar();
+
+    void main_slot_gr1();
+    void main_slot_gr2();
+    void main_slot_gr3();
+    void main_slot_gr4();
+    void main_slot_gr5();
+
+    void main_slot_pasteGr1();
+    void main_slot_pasteGr2();
+    void main_slot_pasteGr3();
+    void main_slot_pasteGr4();
+    void main_slot_pasteGr5();
+
+    void main_slot_resetStatusBarText();
+
 
 private:
     void setCurrentTabNameFromFile(QString asFileName);
@@ -68,30 +84,36 @@ private:
 
     Ui::MainWindow *ui;             //Interfaz de usuario
     QStringList gobFileNames;       //Lista de archivos arrastrados o abiertos
-    int giCurrentFileIndex;         //Índice para el archivo actual que se abrirá
-    int giCurrentTabIndex;          //Índice para la pestaña actual
-    int giTotalTabs;                //Total de pestañas abiertas
+    int giCurrentFileIndex;         //Ãndice para el archivo actual que se abrirÃ¡
+    int giCurrentTabIndex;          //Ãndice para la pestaÃ±a actual
+    int giTotalTabs;                //Total de pestaÃ±as abiertas
     int giTabCharacters;            //Number of tab spaces
-    int giTimerDelay;               //Tiempo de espera en milisegundos para la recarga automática
-    bool gbIsOpenedFile;            //Bandera que indica si se está abriendo un archivo
+    int giTimerDelay;               //Tiempo de espera en milisegundos para la recarga automÃ¡tica
+    bool gbIsOpenedFile;            //Bandera que indica si se estÃ¡ abriendo un archivo
     bool gbIsReloadFile;            //Bandera que indica si un archivo se ha recargado
     bool gbIsAutoreloadEnabled;     //Bandera que indica si la recarga automatica esta activa
-    bool gbSaveCancelled;           //Bandera que indica si se canceló el guardado del archivo
-    QHash<int, QString> gobHash;    //Mapa que almacena índice del tab y ruta de archivo
-    QHash<int, bool> gobIsModifiedTextHash;     //Mapa que almacena índice del tab, y si el archivo correspondiente ha sido modificado
-    SearchDialog *gobSearchDialog;  //Diálogo de buscar y reemplazar
+    bool gbSaveCancelled;           //Bandera que indica si se cancelÃ³ el guardado del archivo
+    QHash<int, QString> gobHash;    //Mapa que almacena Ã­ndice del tab y ruta de archivo
+    QHash<int, bool> gobIsModifiedTextHash;     //Mapa que almacena Ã­ndice del tab, y si el archivo correspondiente ha sido modificado
+    SearchDialog *gobSearchDialog;  //DiÃ¡logo de buscar y reemplazar
     Worker *worker;                 //Objeto para procesar tareas en un hilo separado
     QThread *workerThread;          //Hilo separado del hilo principal
     CustomTextEdit *gobCurrentPlainTextEdit;    //Objeto que almacena el QPlainTextEdit actual
     QFile *gobFile;                 //Objeto que almacena temporalemente un objeto de archivo
     QString gsThemeFile;            //Ruta del archivo del tema usado (style.qss)
-    QTimer *gobTimer;               //Temporizador para recarga automática de archivos
+    QTimer *gobTimer;               //Temporizador para recarga automÃ¡tica de archivos
     QMovie *gobMovie = new QMovie("://reloading.gif");
     QString gsDefaultDir;
     QString gsSavedFont;
     int giSavedFontStyle;
     int giSavedFontPointSize;
     int giDefaultDirCounter;
+    QString gsStatusBarTemporalText;
+    QString gsGr1;
+    QString gsGr2;
+    QString gsGr3;
+    QString gsGr4;
+    QString gsGr5;
 
 protected:
     virtual void dropEvent(QDropEvent *event);

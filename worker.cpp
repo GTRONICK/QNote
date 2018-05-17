52da4700f7cfd85b9f5ecdf7a28b74d1b0ab0537
Jaime Quiroga <gtronick@gmail.com>
2018-05-17 16:59:40 -0500

    Update worker.cpp
    Created a new functionality for file reloading.

--- worker.cpp
+++ worker.cpp
@@ -1,7 +1,10 @@
 #include "worker.h"
 #include <QTextStream>
+#include <QDebug>
 
-Worker::Worker(QObject *parent) : QObject(parent){}
+Worker::Worker(QObject *parent) : QObject(parent){
+    giFileSize = 0;
+}
 
 void Worker::worker_slot_loadFile(QFile *file)
 {
@@ -14,7 +17,29 @@ void Worker::worker_slot_loadFile(QFile *file)
     }
     in.flush();
     file->close();
-
     text.remove(text.lastIndexOf("\n"),1);
     emit worker_signal_appendText(text);
 }
+
+void Worker::worker_slot_tailFile(QFile *file)
+{
+    if(file->size() > giFileSize) {
+        int liDiff = file->size() - giFileSize;
+        if(file->open(QIODevice::ReadOnly | QIODevice::Text)) {
+            if(file->seek(giFileSize)) {
+                QString lsText = QString(file->read(liDiff));
+                emit worker_signal_insertText(lsText);
+                giFileSize = file->size();
+            }
+            file->close();
+        }
+    } else {
+        giFileSize = file->size();
+    }
+}
+
+void Worker::worker_slot_setCurrentFileSize(int aiFileSize)
+{
+    this->giFileSize = aiFileSize;
+}
+

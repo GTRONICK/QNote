b3f7d155968072b861ed29a2129c79bea116baf8
Jaime Quiroga <gtronick@gmail.com>
2018-05-24 16:52:36 -0500

    Improved reload functionality
    Improved reload functionality, and commented some debug outputs

--- worker.cpp
+++ worker.cpp
@@ -8,6 +8,8 @@ Worker::Worker(QObject *parent) : QObject(parent){
 
 void Worker::worker_slot_loadFile(QFile *file)
 {
+    //qDebug() << "Begin worker_slot_loadFile, file: " << file->fileName();
+
     QString line = "";
     QString text = "";
     QTextStream in(file);
@@ -19,11 +21,13 @@ void Worker::worker_slot_loadFile(QFile *file)
     file->close();
     text.remove(text.lastIndexOf("\n"),1);
     emit worker_signal_appendText(text);
+
+    //qDebug() << "End worker_slot_loadFile";
 }
 
 void Worker::worker_slot_tailFile(QFile *file)
 {
-    if(file->size() > giFileSize) {
+    if(file->size() != giFileSize) { //Se cambio de > a != para cargar el archivo aun si su tamaño se ha reducido
         int liDiff = file->size() - giFileSize;
         if(file->open(QIODevice::ReadOnly | QIODevice::Text)) {
             if(file->seek(giFileSize)) {

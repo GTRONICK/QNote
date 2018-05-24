8edd3908f559e945f65c6a5bc71ccf9c3ddedaab
Jaime Quiroga <gtronick@gmail.com>
2018-05-24 16:49:56 -0500

    Reordered code
    Reordered the code and added some new functionalities and performance improvements

--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #define MAX_RECENT 20
+#define TABCHARS    4
 
 #include <QMainWindow>
 #include <QThread>
@@ -36,109 +37,114 @@ signals:
     void main_signal_setCurrentFileSize(int aiFileSize);
 
 private slots:
-    void on_actionOpen_triggered();
     bool on_actionSave_As_triggered();
     bool on_actionSave_triggered();
-    void on_actionAbout_QNote_triggered();
-    void on_actionAbout_QT_triggered();
-    void on_tabWidget_tabCloseRequested(int index);
-    void on_actionNew_Tab_triggered();
-    void main_slot_tabChanged(int aIndex);
-    void main_slot_tabMoved(int from, int to);
-    void main_slot_textChanged();
-    void on_actionClose_Tab_triggered();
-    void main_slot_getTextEditText();
-    void main_slot_resetCursor();
-    void on_actionFind_Replace_triggered();
     void main_slot_appendText(QString asText);
-    void main_slot_insertText(QString asText);
-    void main_slot_processDropEvent(QDropEvent *event);
-    void on_actionReload_file_triggered();
-    void on_actionWord_wrap_toggled(bool arg1);
-    void on_actionGo_to_line_triggered();
-    void on_actionLoad_theme_triggered();
-    void on_actionSystem_theme_triggered();
-    void on_actionAuto_Reload_tail_f_toggled(bool arg1);
-    void on_actionAuto_Reload_delay_triggered();
     void main_slot_currentLineChanged();
-    void on_actionFont_triggered();
-    void on_actionAlways_on_top_triggered(bool checked);
-    void main_slot_showHideMenuBar();
     void main_slot_dontShowAgain(bool abValue);
-
+    void main_slot_getTextEditText();
     void main_slot_gr1();
     void main_slot_gr2();
     void main_slot_gr3();
     void main_slot_gr4();
-
+    void main_slot_insertText(QString asText);
+    void main_slot_loadFileFromAction(QAction *aobAction);
+    void main_slot_openFileLocation();
     void main_slot_pasteGr1();
     void main_slot_pasteGr2();
     void main_slot_pasteGr3();
     void main_slot_pasteGr4();
-
+    void main_slot_processDropEvent(QDropEvent *event);
+    void main_slot_resetCursor();
     void main_slot_resetStatusBarText();
-    void on_statusBar_linkActivated(const QString &link);
-    void main_slot_loadFileFromAction(QAction *aobAction);
-    void main_slot_openFileLocation();
-
+    void main_slot_showHideMenuBar();
+    void main_slot_tabChanged(int aIndex);
+    void main_slot_tabMoved(int from, int to);
+    void main_slot_tailFile();
+    void main_slot_textChanged();
+    void on_actionAbout_QNote_triggered();
+    void on_actionAbout_QT_triggered();
+    void on_actionAlways_on_top_triggered(bool checked);
+    void on_actionAuto_Reload_delay_triggered();
+    void on_actionAuto_Reload_tail_f_toggled(bool arg1);
+    void on_actionClose_Tab_triggered();
     void on_actionErase_and_save_triggered();
+    void on_actionFind_Replace_triggered();
+    void on_actionFont_triggered();
+    void on_actionGo_to_line_triggered();
+    void on_actionLoad_theme_triggered();
+    void on_actionNew_Tab_triggered();
+    void on_actionOpen_triggered();
+    void on_actionReload_file_triggered();
     void on_actionReset_alerts_triggered();
     void on_actionSet_Maximun_file_size_triggered();
+    void on_actionSystem_theme_triggered();
     void on_actionTo_UPERCASE_triggered();
     void on_actionTo_lowercase_triggered();
+    void on_actionWord_wrap_toggled(bool arg1);
+    void on_statusBar_linkActivated(const QString &link);
+    void on_tabWidget_tabCloseRequested(int index);
 
-    void main_slot_tailFile();
+    void on_actionErase_and_save_2_triggered();
 
 private:
-    void addRecentFiles();
-    void setCurrentTabNameFromFile(QString asFileName);
-    void checkIfUnsaved(int index);
     bool checkFileExist(QString asFileName);
-    int checkFileSize(QString asFileName);
+    bool loadConfig();
+    bool saveConfig();
+    bool saveFile(QString asFileName, QString asText);
+    //int checkFileSize(QString asFileName);
+    void checkIfUnsaved(int index);
     void closeTab(int index);
     void loadFile(QString asFileName);
-    bool saveFile(QString asFileName, QString asText);
-    bool saveConfig();
-    bool loadConfig();
+    void setCurrentTabNameFromFile(QString asFileName);
+    void addRecentFiles();
+    QStringList removeDuplicates(QStringList aobList);
 
     Ui::MainWindow *ui;             //Interfaz de usuario
-    QStringList gobFileNames;       //Lista de archivos arrastrados o abiertos
+
+    float gfMaxFileSize;
+
     int giCurrentFileIndex;         //indice para el archivo actual que se abrirá
     int giCurrentTabIndex;          //indice para la pestaña actual
-    int giTotalTabs;                //Total de pestañas abiertas
-    int giTabCharacters;            //Number of tab spaces
-    int giTimerDelay;               //Tiempo de espera en milisegundos para la recarga automática
-    int giRecentFilePos;            //Contador de posicion del archivo actual
-    int giRecentAux;
-    int giOpenWithFlag;
-    int giSavedFontStyle;
-    int giSavedFontPointSize;
     int giDefaultDirCounter;
-    float gfMaxFileSize;
+    int giOpenWithFlag;
+    int giRecentAux;
+    int giRecentFilePos;            //Contador de posicion del archivo actual
+    int giSavedFontPointSize;
+    int giSavedFontStyle;
+    int giTimerDelay;               //Tiempo de espera en milisegundos para la recarga automática
+    int giTotalTabs;                //Total de pestañas abiertas
+
     bool gbShowEraseAndSaveMessageBox;  //Bandera que indica se se muestra o no el QMessageBox de Borrado, guardado y recarga.
     bool gbIsOpenedFile;            //Bandera que indica si se está abriendo un archivo
     bool gbIsReloadFile;            //Bandera que indica si un archivo se ha recargado
     bool gbIsAutoreloadEnabled;     //Bandera que indica si la recarga automatica esta activa
     bool gbSaveCancelled;           //Bandera que indica si se canceló el guardado del archivo
+
     QHash<int, QString> gobHash;    //Mapa que almacena índice del tab y ruta de archivo
     QHash<int, bool> gobIsModifiedTextHash;     //Mapa que almacena índice del tab, y si el archivo correspondiente ha sido modificado
-    SearchDialog *gobSearchDialog;  //Diálogo de buscar y reemplazar
-    Worker *worker;                 //Objeto para procesar tareas en un hilo separado
-    QThread *workerThread;          //Hilo separado del hilo principal
-    CustomTextEdit *gobCurrentPlainTextEdit;    //Objeto que almacena el QPlainTextEdit actual
-    QFile *gobFile;                 //Objeto que almacena temporalemente un objeto de archivo
-    QTimer *gobTimer;               //Temporizador para recarga automática de archivos
-    QMovie *gobMovie;
-    QString gsThemeFile;            //Ruta del archivo del tema usado (style.qss)
+
     QString gsDefaultDir;
-    QString gsSavedFont;
-    QString gsStatusBarTemporalText;
     QString gsGr1;
     QString gsGr2;
     QString gsGr3;
     QString gsGr4;
     QString gsGr5;
+    QString gsSavedFont;
+    QString gsStatusBarTemporalText;
+    QString gsThemeFile;            //Ruta del archivo del tema usado (style.qss)
+    QString gsStatusBarColor;
+
+    QStringList gobFileNames;       //Lista de archivos arrastrados o abiertos
     QStringList gobRecentFiles;
+
+    SearchDialog *gobSearchDialog;  //Diálogo de buscar y reemplazar
+    CustomTextEdit *gobCurrentPlainTextEdit;    //Objeto que almacena el QPlainTextEdit actual
+    QMovie *gobMovie;
+    QThread *workerThread;          //Hilo separado del hilo principal
+    QTimer *gobTimer;               //Temporizador para recarga automática de archivos
+    Worker *worker;                 //
+
 
 protected:
     virtual void dropEvent(QDropEvent *event);
